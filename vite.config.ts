c666d3f2b93b491dc740039d2489bc2db928b1c1
Shak <sanshakurt@gmail.com>
2025-10-30 13:56:10 +0330

    remove gh-pages package and configs of it on main branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "node:path";
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: "/expression-compiler",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
